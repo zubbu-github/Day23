d705149812a811f80f1477dbe9995144f9753a7a
LENOVO <mdzubeir2468@gmail.com>
2022-03-11 11:52:24 +0530

    day 23 Case study first commit

---
+++ Day23/src/day23/Demo.java
@@ -0,0 +1,29 @@
+package day23;
+
+class Goshopping{
+	 private int PrimeAcc,NormalAcc;
+       public int getPrimeAcc()
+   {
+		return PrimeAcc;
+	}
+	public void setPrimeAcc(int primeAcc) {
+		PrimeAcc = primeAcc;
+	}
+	public int getNormalAcc() {
+		return NormalAcc;
+	}
+	public void setNormalAcc(int normalAcc) {
+		NormalAcc = normalAcc;
+	}
+		String  AccNm;
+       float charges;    
+}
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Goshopping PrimeAcc,NormalAccc;
+  
+	}
+
+}
